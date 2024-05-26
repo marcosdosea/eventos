d11276ad34936cc279542b20a985ffddd8ae3fe5
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2024-05-26 20:28:06 -0300

    Adicionar IEventoService

---
+++ Codigo/Evento/Core/Service/IEventoService.cs
@@ -0,0 +1,23 @@
+﻿using Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Core.Service
+{
+    public interface IEventoService
+    {
+        public uint Inserir(Evento evento);
+        public void Atualizar(Evento evento);
+        public void Remover(uint Id);
+
+        public Evento Obter(uint Id);
+
+        public IEnumerable<Evento> GetAll();
+
+        public IEnumerable<Evento> GetByNome(string Nome);
+    }
+}
