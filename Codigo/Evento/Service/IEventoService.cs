db7609af8350a1a3317b3a5ec7913e20f7a7cfa4
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2024-05-27 23:52:49 -0300

    Correção

---
+++ Codigo/Evento/Service/IEventoService.cs
@@ -0,0 +1,18 @@
+﻿using Core;
+using Core.DTO;
+
+namespace Core.Service
+{
+    public interface IEventoService
+    {
+        uint Create(Evento evento);
+        void Edit(Evento evento);
+        void Delete(uint Id);
+
+        Evento Get(uint Id);
+
+        IEnumerable<Evento> GetAll();
+
+        IEnumerable<EventoDTO> GetByNome(string Nome);
+    }
+}
