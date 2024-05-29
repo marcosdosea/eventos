c7b28705237cd07c754eff9c193b060b349136f3
muzanpvp <mkibutsuji4@gmail.com>
2024-05-28 22:50:27 -0300

    correção Program.cs removido linha 22

--- Codigo/Evento/EventoWeb/Program.cs
+++ Codigo/Evento/EventoWeb/Program.cs
@@ -19,7 +19,6 @@ namespace EventoWeb
 
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             builder.Services.AddTransient<ITipoInscricaoService, TipoInscricaoService>();
-            builder.Services.AddTransient<IAreaInteresseService, AreaInteresseService>();
             builder.Services.AddTransient<ISubeventoService, SubeventoService>();
             builder.Services.AddTransient<IEventoService, EventoService>();
             builder.Services.AddTransient<IModelocrachaService, ModelocrachaService>();
