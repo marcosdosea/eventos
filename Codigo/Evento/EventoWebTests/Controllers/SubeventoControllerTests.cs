ff0ab5e71ff3621e9c49a3db55dcee18115ac8b0
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2024-06-03 07:48:13 -0300

    ADD SubeventoControllerTests

---
+++ Codigo/Evento/EventoWebTests/Controllers/SubeventoControllerTests.cs
@@ -0,0 +1,331 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core;
+using Microsoft.AspNetCore.Mvc;
+using EventoWeb.Models;
+using EventoWeb.Mappers;
+using Core.Service;
+using Moq;
+using AutoMapper;
+namespace EventoWeb.Controllers.Tests
+{
+    [TestClass()]
+    public class SubeventoControllerTests
+    {
+        private static SubeventoController controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Arrange
+            var mockService = new Mock<ISubeventoService>();
+
+            IMapper mapper = new MapperConfiguration(cfg =>
+            cfg.AddProfile(new SubeventoProfile())).CreateMapper();
+
+            mockService.Setup(service => service.GetAll())
+                .Returns(GetTestSubeventos());
+            mockService.Setup(service => service.Get(1))
+                .Returns(GetTargetSubevento());
+            mockService.Setup(service => service.Create(It.IsAny<Subevento>()))
+                .Verifiable();
+            controller = new SubeventoController(mockService.Object, mapper);
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(List<SubeventoModel>));
+
+            List<SubeventoModel>? lista = (List<SubeventoModel>)viewResult.ViewData.Model;
+            Assert.AreEqual(3, lista.Count);
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            // Act
+            var result = controller.Details(1);
+
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(SubeventoModel));
+            SubeventoModel subeventoModel = (SubeventoModel)viewResult.ViewData.Model;
+            Assert.AreEqual((uint)1, subeventoModel.IdEvento);
+            Assert.AreEqual("SEMINFO", subeventoModel.Nome);
+            Assert.AreEqual("Evento para a semana da tecnologia", subeventoModel.Descricao);
+            Assert.AreEqual((sbyte)1, subeventoModel.InscricaoGratuita);
+            Assert.AreEqual("A", subeventoModel.Status);
+            Assert.AreEqual(DateTime.Parse("2024-09-02 07:30:00"), subeventoModel.DataInicioInscricao);
+            Assert.AreEqual(DateTime.Parse("2024-09-07 12:30:00"), subeventoModel.DataFimInscricao);
+            Assert.AreEqual((decimal)0, subeventoModel.ValorInscricao);
+            Assert.AreEqual((sbyte)1, subeventoModel.PossuiCertificado);
+            Assert.AreEqual((decimal)1, subeventoModel.FrequenciaMinimaCertificado);
+            Assert.AreEqual((int)1, subeventoModel.IdTipoEvento);
+            Assert.AreEqual((int)100, subeventoModel.VagasOfertadas);
+            Assert.AreEqual((int)35, subeventoModel.VagasReservadas);
+            Assert.AreEqual((int)65, subeventoModel.VagasDisponiveis);
+            Assert.AreEqual((int)4, subeventoModel.CargaHoraria);
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            // Act
+            var result = controller.Create();
+
+            // Assert 
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void CreateTest_Valid()
+        {
+            // Act
+            var result = controller.Create(GetNewSubevento());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+        public void CreateTest_Invalid()
+        {
+            // Arrange
+            controller.ModelState.AddModelError("Nome", "Nome do Subevento é obrigatório");
+
+            // Act
+            var result = controller.Create(GetNewSubevento());
+
+            // Assert
+            Assert.AreEqual(1, controller.ModelState.ErrorCount);
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+
+        [TestMethod()]
+        public void EditTest_Get_Valid()
+        {
+            // Act
+            var result = controller.Edit(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(SubeventoModel));
+            SubeventoModel subeventoModel = (SubeventoModel)viewResult.ViewData.Model;
+            Assert.AreEqual((uint)1, subeventoModel.IdEvento);
+            Assert.AreEqual("SEMINFO", subeventoModel.Nome);
+            Assert.AreEqual("Evento para a semana da tecnologia", subeventoModel.Descricao);
+            Assert.AreEqual((sbyte)1, subeventoModel.InscricaoGratuita);
+            Assert.AreEqual("A", subeventoModel.Status);
+            Assert.AreEqual(DateTime.Parse("2024-09-02 07:30:00"), subeventoModel.DataInicioInscricao);
+            Assert.AreEqual(DateTime.Parse("2024-09-07 12:30:00"), subeventoModel.DataFimInscricao);
+            Assert.AreEqual((decimal)0, subeventoModel.ValorInscricao);
+            Assert.AreEqual((sbyte)1, subeventoModel.PossuiCertificado);
+            Assert.AreEqual((decimal)1, subeventoModel.FrequenciaMinimaCertificado);
+            Assert.AreEqual((int)1, subeventoModel.IdTipoEvento);
+            Assert.AreEqual((int)100, subeventoModel.VagasOfertadas);
+            Assert.AreEqual((int)35, subeventoModel.VagasReservadas);
+            Assert.AreEqual((int)65, subeventoModel.VagasDisponiveis);
+            Assert.AreEqual((int)4, subeventoModel.CargaHoraria);
+        }
+
+        [TestMethod()]
+        public void EditTest_Post_Valid()
+        {
+            // Act
+            var result = controller.Edit(GetTargetSubeventoModel().Id, GetTargetSubeventoModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [TestMethod()]
+        public void DeleteTest_Post_Valid()
+        {
+            // Act
+            var result = controller.Delete(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(SubeventoModel));
+            SubeventoModel subeventoModel = (SubeventoModel)viewResult.ViewData.Model;
+            Assert.AreEqual((uint)1, subeventoModel.IdEvento);
+            Assert.AreEqual("SEMINFO", subeventoModel.Nome);
+            Assert.AreEqual("Evento para a semana da tecnologia", subeventoModel.Descricao);
+            Assert.AreEqual((sbyte)1, subeventoModel.InscricaoGratuita);
+            Assert.AreEqual("A", subeventoModel.Status);
+            Assert.AreEqual(DateTime.Parse("2024-09-02 07:30:00"), subeventoModel.DataInicioInscricao);
+            Assert.AreEqual(DateTime.Parse("2024-09-07 12:30:00"), subeventoModel.DataFimInscricao);
+            Assert.AreEqual((decimal)0, subeventoModel.ValorInscricao);
+            Assert.AreEqual((sbyte)1, subeventoModel.PossuiCertificado);
+            Assert.AreEqual((decimal)1, subeventoModel.FrequenciaMinimaCertificado);
+            Assert.AreEqual((int)1, subeventoModel.IdTipoEvento);
+            Assert.AreEqual((int)100, subeventoModel.VagasOfertadas);
+            Assert.AreEqual((int)35, subeventoModel.VagasReservadas);
+            Assert.AreEqual((int)65, subeventoModel.VagasDisponiveis);
+            Assert.AreEqual((int)4, subeventoModel.CargaHoraria);
+        }
+
+        [TestMethod()]
+        public void DeleteTest_Get_Valid()
+        {
+            // Act
+            var result = controller.Delete(GetTargetSubeventoModel().Id, GetTargetSubeventoModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        private SubeventoModel GetNewSubevento()
+        {
+            return new SubeventoModel
+            {
+                Id = 1,
+                IdEvento = 1,
+                Nome = "SEMINFO",
+                Descricao = "Evento para a semana da tecnologia",
+                InscricaoGratuita = 1,
+                Status = "A",
+                DataInicioInscricao = new DateTime(2024, 09, 2, 7, 30, 0),
+                DataFimInscricao = new DateTime(2024, 09, 7, 12, 30, 0),
+                ValorInscricao = 0,
+                PossuiCertificado = 1,
+                FrequenciaMinimaCertificado = 1,
+                IdTipoEvento = 1,
+                VagasOfertadas = 100,
+                VagasReservadas = 35,
+                VagasDisponiveis = 65,
+                CargaHoraria = 4,
+            };
+        }
+        private static Subevento GetTargetSubevento()
+        {
+            return new Subevento
+            {
+                Id = 1,
+                IdEvento = 1,
+                Nome = "SEMINFO",
+                Descricao = "Evento para a semana da tecnologia",
+                InscricaoGratuita = 1,
+                Status = "A",
+                DataInicioInscricao = new DateTime(2024, 09, 2, 7, 30, 0),
+                DataFimInscricao = new DateTime(2024, 09, 7, 12, 30, 0),
+                ValorInscricao = 0,
+                PossuiCertificado = 1,
+                FrequenciaMinimaCertificado = 1,
+                IdTipoEvento = 1,
+                VagasOfertadas = 100,
+                VagasReservadas = 35,
+                VagasDisponiveis = 65,
+                CargaHoraria = 4,
+            };
+        }
+
+        private SubeventoModel GetTargetSubeventoModel()
+        {
+            return new SubeventoModel
+            {
+                Id = 1,
+                IdEvento = 1,
+                Nome = "SEMINFO",
+                Descricao = "Evento para a semana da tecnologia",
+                InscricaoGratuita = 1,
+                Status = "A",
+                DataInicioInscricao = new DateTime(2024, 09, 2, 7, 30, 0),
+                DataFimInscricao = new DateTime(2024, 09, 7, 12, 30, 0),
+                ValorInscricao = 0,
+                PossuiCertificado = 1,
+                FrequenciaMinimaCertificado = 1,
+                IdTipoEvento = 1,
+                VagasOfertadas = 100,
+                VagasReservadas = 35,
+                VagasDisponiveis = 65,
+                CargaHoraria = 4,
+            };
+        }
+
+        private IEnumerable<Subevento> GetTestSubeventos()
+        {
+            return new List<Subevento>
+            {
+                new Subevento
+                {
+                    Id = 1,
+                    IdEvento = 1,
+                    Nome = "SEMINFO",
+                    Descricao = "Evento para a semana da tecnologia",
+                    InscricaoGratuita = 1,
+                    Status = "A",
+                    DataInicioInscricao = new DateTime(2024, 09, 2, 7, 30, 0),
+                    DataFimInscricao = new DateTime(2024, 09, 7, 12, 30, 0),
+                    ValorInscricao = 0,
+                    PossuiCertificado = 1,
+                    FrequenciaMinimaCertificado = 1,
+                    IdTipoEvento = 1,
+                    VagasOfertadas = 100,
+                    VagasReservadas = 35,
+                    VagasDisponiveis = 65,
+                    CargaHoraria = 4,
+                    },
+                new Subevento
+                {
+                        Id = 3,
+                        IdEvento = 1,
+                        Nome = "SEMAC",
+                        Descricao = "Semana academica de cursos",
+                        InscricaoGratuita = 1,
+                        Status = "F",
+                        DataInicioInscricao = new DateTime(2024, 02, 2, 7, 30, 0),
+                        DataFimInscricao = new DateTime(2024, 02, 7, 12, 30, 0),
+                        ValorInscricao = 0,
+                        PossuiCertificado = 1,
+                        FrequenciaMinimaCertificado = 1,
+                        IdTipoEvento = 1,
+                        VagasOfertadas = 100,
+                        VagasReservadas = 35,
+                        VagasDisponiveis = 65,
+                        CargaHoraria = 4,
+                    },
+                new Subevento
+                {
+                        Id = 5,
+                        IdEvento = 1,
+                        Nome = "Balada do DJ Ikaruz",
+                        Descricao = "Festa Fechada",
+                        InscricaoGratuita = 1,
+                        Status = "C",
+                        DataInicioInscricao = new DateTime(2024, 09, 2, 7, 30, 0),
+                        DataFimInscricao = new DateTime(2024, 09, 3, 7, 30, 0),
+                        ValorInscricao = 0,
+                        PossuiCertificado = 0,
+                        FrequenciaMinimaCertificado = 0,
+                        IdTipoEvento = 3,
+                        VagasOfertadas = 100,
+                        VagasReservadas = 35,
+                        VagasDisponiveis = 65,
+                        CargaHoraria = 12,
+                    },
+            };
+        }
+    }
+}
