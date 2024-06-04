83fc00f9b3f4490c4528d104315a241fd90b9182
joauxx <joaosodre23@gmail.com>
2024-06-04 09:02:59 -0300

    Desenvolvimento Back-end do manter pessoa

---
+++ Codigo/Evento/Service/PessoaService.cs
@@ -0,0 +1,88 @@
+using Core;
+using Core.DTO;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service;
+
+public class PessoaService : IPessoaService
+{
+    /// <summary>
+    /// Manter dados de pessoa no banco de dados
+    /// </summary>
+    private readonly EventoContext _context;
+
+    public PessoaService(EventoContext context)
+    {
+        this._context = context;
+    }
+    /// <summary>
+    /// Insere uma nova pessoa na base de dados
+    /// </summary>
+    /// <param name="pessoa">dados de pessoa</param>
+    /// <returns></returns>
+    public uint Create(Pessoa pessoa)
+    {
+        _context.Add(pessoa);
+        _context.SaveChanges();
+        return pessoa.Id;
+    }
+    
+    /// <summary>
+    /// Edita uma pessoa na base de dados
+    /// </summary>
+    /// <param name="pessoa">dados de pessoa</param>
+    /// <returns></returns>
+    public void Edit(Pessoa pessoa)
+    {
+        _context.Update(pessoa);
+        _context.SaveChanges();
+    }
+    /// <summary>
+    /// Exclui uma pessoa na base de dados
+    /// </summary>
+    /// <param name="id">dados de pessoa</param>
+    /// <returns></returns>
+    public void Delete(uint id)
+    {
+        var pessoa = _context.Pessoas.Find(id);
+        _context.Remove(pessoa);
+        _context.SaveChanges();
+    }
+    /// <summary>
+    /// Obtém uma pessoa específica por id
+    /// </summary>
+    /// <param name="id">dados de pessoa</param>
+    /// <returns></returns>
+    public Pessoa Get(uint id)
+    {
+        return _context.Pessoas.Find(id);
+    }
+    /// <summary>
+    /// Obtém todas pessoas
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<Pessoa> GetAll()
+    {
+        return _context.Pessoas.AsNoTracking();
+    }
+    
+    /// <summary>
+    /// Obtém uma pessoa específica por cpf
+    /// </summary>
+    /// <param name="cpf">dados de pessoa</param>
+    /// <returns></returns>
+    public IEnumerable<PessoaDTO> GetByCpf(string cpf)
+    {
+        var query = from pessoa in _context.Pessoas
+            where pessoa.Cpf.StartsWith(cpf)
+            select new Core.DTO.PessoaDTO()
+            {
+                Id = pessoa.Id,
+                Nome = pessoa.Nome
+            };
+        return query;
+    }
+}
+
+
