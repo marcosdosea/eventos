1b53eba179826cfe791e8d0f5ff27b3ac2f7c899
Ikaruz <ikaruz.music@hotmail.com>
2024-06-03 22:36:50 -0300

    list view
    Criada padrões de projeto e tabela

--- Codigo/Evento/Core/DTO/EventoDTO.cs
+++ Codigo/Evento/Core/DTO/EventoDTO.cs
@@ -16,6 +16,19 @@
         /// </summary>
         public string Status { get; set; } = null!;
 
+        public string StatusDescricao
+        {
+            get
+            {
+                if (Status.Equals("C"))
+                    return "Em Cadastro";
+                else if (Status.Equals("A"))
+                    return "Aberto";
+                else
+                    return "Finalizado";
+            }
+        }
+
         public DateTime DataInicio { get; set; }
 
         public DateTime DataFim { get; set; }
