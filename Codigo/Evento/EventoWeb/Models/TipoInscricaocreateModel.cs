90a1d5307a0f0567c05829b36f53b559fdea010e
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2024-06-11 17:05:34 -0300

    Criação de um Model para auxiliar TipoInscricao

---
+++ Codigo/Evento/EventoWeb/Models/TipoInscricaocreateModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EventoWeb.Models
+{
+    public class TipoInscricaocreateModel
+    {
+        public TipoInscricaoModel TipoInscricao { get; set; }
+        public SelectList Evento { get; set; }
+
+    }
+}
