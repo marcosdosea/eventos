15c64f18aafed0ab7ef0f8ece18778e4bd3a1c16
Ikaruz <ikaruz.music@hotmail.com>
2024-06-15 12:36:39 -0300

    finalizado front Manter Gestor
    - Tem um Js comentado pois falta ajustes no back-end para ele funcionar melhor

---
+++ Codigo/Evento/EventoWeb/Models/DeleteModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EventoWeb.Models
+{
+    public class DeleteModel
+    {
+        public string ModalId { get; set; }
+        public string ActionName { get; set; }
+        public string ControllerName { get; set; }
+        public object RouteValues { get; set; }
+        public string ConfirmMessage { get; set; }
+        public string ItemName { get; set; }
+    }
+
+}
