b4c4dea5c3c3d5826e449683547d101ce2e394d6
Ikaruz <ikaruz.music@hotmail.com>
2024-08-06 20:00:02 -0300

    adicionado campos em registrar
    Falta apenas colocar a relação do usuário com o papel dele

--- Codigo/Evento/EventoWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Codigo/Evento/EventoWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,6 +19,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using Util;
+using Core.Service;
+using Core;
 
 namespace EventoWeb.Areas.Identity.Pages.Account
 {
@@ -30,27 +33,29 @@ namespace EventoWeb.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<UsuarioIdentity> _emailStore;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+		private readonly IPessoaService _pessoaService;
 
-        public RegisterModel(
-            UserManager<UsuarioIdentity> userManager,
-            IUserStore<UsuarioIdentity> userStore,
-            SignInManager<UsuarioIdentity> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
-        {
-            _userManager = userManager;
-            _userStore = userStore;
-            _emailStore = GetEmailStore();
-            _signInManager = signInManager;
-            _logger = logger;
-            _emailSender = emailSender;
-        }
+		public RegisterModel(
+			UserManager<UsuarioIdentity> userManager,
+			IUserStore<UsuarioIdentity> userStore,
+			SignInManager<UsuarioIdentity> signInManager,
+			ILogger<RegisterModel> logger,
+			IEmailSender emailSender,
+			IPessoaService pessoaService)
+		{
+			_userManager = userManager;
+			_userStore = userStore;
+			_signInManager = signInManager;
+			_logger = logger;
+			_emailSender = emailSender;
+			_pessoaService = pessoaService;
+		}
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
-        [BindProperty]
+		/// <summary>
+		///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+		///     directly from your code. This API may change or be removed in future releases.
+		/// </summary>
+		[BindProperty]
         public InputModel Input { get; set; }
 
         /// <summary>
@@ -97,64 +102,77 @@ namespace EventoWeb.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
-        }
+
+			[Required]
+			[Display(Name = "Nome")]
+			public string Nome { get; set; }
+
+			[Required]
+			[Display(Name = "CPF")]
+			public string CPF { get; set; }
+		}
 
 
-        public async Task OnGetAsync(string returnUrl = null)
-        {
-            ReturnUrl = returnUrl;
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-        }
+		public async Task OnGetAsync(string returnUrl = null)
+		{
+			ReturnUrl = returnUrl;
+			ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+		}
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        {
-            returnUrl ??= Url.Content("~/");
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-            if (ModelState.IsValid)
-            {
-                var user = CreateUser();
+		public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+		{
+			returnUrl = returnUrl ?? Url.Content("~/");
+			ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+			if (ModelState.IsValid)
+			{
+				var user = new UsuarioIdentity { UserName = Input.CPF, Email = Input.Email };
+				var result = await _userManager.CreateAsync(user, Input.Password);
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                var result = await _userManager.CreateAsync(user, Input.Password);
+				if (result.Succeeded)
+				{
+					_logger.LogInformation("User created a new account with password.");
 
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User created a new account with password.");
+					var pessoaResult = _pessoaService.Create(new Pessoa
+					{
+						Nome = Input.Nome,
+						NomeCracha = Input.Nome,
+						Cpf = Input.CPF,
+						Email = Input.Email
+					});
 
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
+					var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+					code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+					var callbackUrl = Url.Page(
+						"/Account/ConfirmEmail",
+						pageHandler: null,
+						values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+						protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-                    
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
-                    }
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-            }
+					await _emailSender.SendEmailAsync(Input.Email, "Confirme seu email",
+						$"Por favor, confirme sua conta <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicando aqui</a>.");
 
-            // If we got this far, something failed, redisplay form
-            return Page();
-        }
+					if (_userManager.Options.SignIn.RequireConfirmedAccount)
+					{
+						return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+					}
+					else
+					{
+						await _signInManager.SignInAsync(user, isPersistent: false);
+						return LocalRedirect(returnUrl);
+					}
+				}
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError(string.Empty, error.Description);
+				}
+			}
 
-        private UsuarioIdentity CreateUser()
+			// If we got this far, something failed, redisplay form
+			return Page();
+		}
+
+
+		private UsuarioIdentity CreateUser()
         {
             try
             {
