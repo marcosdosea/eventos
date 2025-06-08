a5c10fc796a45d89c37912ceeeec4765ccf482c3
Alexandre Moreira <xandemoreira1505@gmail.com>
2025-06-08 12:59:15 -0300

    Foi corrigido o ColaboradorModel.cs

--- Codigo/Evento/EventoWeb/Models/ColaboradorModel.cs
+++ Codigo/Evento/EventoWeb/Models/ColaboradorModel.cs
@@ -5,7 +5,22 @@ namespace EventoWeb.Models;
 
 public class ColaboradorModel
 {
-    public PessoaModel? Colaborador { get; set; }
-    
+    public PessoaModel Colaborador { get; set; }
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string NomeCracha { get; set; }
+    public string Cpf { get; set; }
+    public string Sexo { get; set; }
+    public string Email { get; set; }
+    public string Telefone1 { get; set; }
+    public string Telefone2 { get; set; }
+    public string Cep { get; set; }
+    public string Estado { get; set; }
+    public string Cidade { get; set; }
+    public string Bairro { get; set; }
+    public string Rua { get; set; }
+    public string Numero { get; set; }
+    public string Complemento { get; set; }
+
     public IEnumerable<PessoaSimpleDTO>? Colaboradores { get; set; }
 }
