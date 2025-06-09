7a88c608bfe84666ac89c8468b16c78efafbd0fc
Felip3D3V <143296850+Felip3D3V@users.noreply.github.com>
2025-06-09 16:15:46 -0300

    Implementado Interface de Serviços de ParticipacaoPessoaSubEvento # close

---
+++ Codigo/Evento/Core/Service/IParticipacaoPessoaSubEventoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IParticipacaoPessoaSubEventoService
+    {
+        uint Create(Participacaopessoasubevento participacao);
+        void Update(Participacaopessoasubevento participacao);
+        void Delete(uint id);
+        IEnumerable<Participacaopessoasubevento> GetBySubEvento(uint idSubEvento);
+        IEnumerable<Participacaopessoasubevento> GetByPessoa(uint idPessoa);
+    }
+}
