2222197bc64d8516f9ce544fe11c0168adfbef5c
Felip3D3V <143296850+Felip3D3V@users.noreply.github.com>
2025-08-07 09:31:55 -0300

    Adicionado Mappers Inscricaopessoaevento closes #467

--- Codigo/Evento/Core/DTO/InscricaopessoaeventoDTO.cs
+++ Codigo/Evento/Core/DTO/InscricaopessoaeventoDTO.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core.DTO
+﻿public class InscricaopessoaeventoDTO
 {
-    internal class InscricaopessoaeventoDTO
-    {
-    }
+    public uint Id { get; set; }
+    public uint IdPessoa { get; set; }
+    public uint IdEvento { get; set; }
+    public int IdPapel { get; set; }
+    public uint? IdTipoInscricao { get; set; }
+    public DateTime DataInscricao { get; set; }
+    public decimal ValorTotal { get; set; }
+    public string Status { get; set; } = null!;
+    public decimal FrequenciaFinal { get; set; }
+    public string? NomeCracha { get; set; }
+    // Adicione outros campos se precisar (ex: nomes de navegação)
 }
---
+++ Codigo/Evento/EventoWeb/Mappers/InscricaopessoaeventoProfile.cs
@@ -0,0 +1,41 @@
+﻿// EventoWeb/Mappers/InscricaopessoaeventoProfile.cs
+using AutoMapper;
+using Core;
+using Core.DTO;
+
+namespace EventoWeb.Mappers
+{
+    public class InscricaopessoaeventoProfile : Profile
+    {
+        public InscricaopessoaeventoProfile()
+        {
+            // Mapeia entidade para DTO
+            CreateMap<Inscricaopessoaevento, InscricaopessoaeventoDTO>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.IdPessoa, opt => opt.MapFrom(src => src.IdPessoa))
+                .ForMember(dest => dest.IdEvento, opt => opt.MapFrom(src => src.IdEvento))
+                .ForMember(dest => dest.IdPapel, opt => opt.MapFrom(src => src.IdPapel))
+                .ForMember(dest => dest.IdTipoInscricao, opt => opt.MapFrom(src => src.IdTipoInscricao))
+                .ForMember(dest => dest.DataInscricao, opt => opt.MapFrom(src => src.DataInscricao))
+                .ForMember(dest => dest.ValorTotal, opt => opt.MapFrom(src => src.ValorTotal))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
+                .ForMember(dest => dest.FrequenciaFinal, opt => opt.MapFrom(src => src.FrequenciaFinal))
+                .ForMember(dest => dest.NomeCracha, opt => opt.MapFrom(src => src.NomeCracha));
+            // Se quiser incluir campos de navegação (ex: nome do evento), pode adicionar aqui:
+            //.ForMember(dest => dest.NomeEvento, opt => opt.MapFrom(src => src.IdEventoNavigation.Nome));
+
+            // Mapeia DTO para entidade
+            CreateMap<InscricaopessoaeventoDTO, Inscricaopessoaevento>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.IdPessoa, opt => opt.MapFrom(src => src.IdPessoa))
+                .ForMember(dest => dest.IdEvento, opt => opt.MapFrom(src => src.IdEvento))
+                .ForMember(dest => dest.IdPapel, opt => opt.MapFrom(src => src.IdPapel))
+                .ForMember(dest => dest.IdTipoInscricao, opt => opt.MapFrom(src => src.IdTipoInscricao))
+                .ForMember(dest => dest.DataInscricao, opt => opt.MapFrom(src => src.DataInscricao))
+                .ForMember(dest => dest.ValorTotal, opt => opt.MapFrom(src => src.ValorTotal))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status))
+                .ForMember(dest => dest.FrequenciaFinal, opt => opt.MapFrom(src => src.FrequenciaFinal))
+                .ForMember(dest => dest.NomeCracha, opt => opt.MapFrom(src => src.NomeCracha));
+        }
+    }
+}
