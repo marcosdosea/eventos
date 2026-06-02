c225f8c36394efeb65b71779602a47e7b71bb479
Ana Maria De Carvalho Mendonça <134330474+annymarry51@users.noreply.github.com>
2026-06-02 15:30:34 -0300

    Removido a permissão de um Administrador adicionar um gestor.

--- Codigo/Evento/EventoWeb/Controllers/EventoController.cs
+++ Codigo/Evento/EventoWeb/Controllers/EventoController.cs
@@ -287,7 +287,7 @@ namespace EventoWeb.Controllers
 		}
 		
 		
-		[Authorize(Roles = "ADMINISTRADOR,GESTOR,COLABORADOR")]
+		[Authorize(Roles = "GESTOR")]
         // GET: EventoController/CreateColaborador
         [HttpGet]
         [Route("CreateColaborador")]
@@ -295,7 +295,7 @@ namespace EventoWeb.Controllers
 		{
 			
 			var gestor = _inscricaoService.GetGestorInEvent(User.Identity.Name, idEvento);
-			if(User.IsInRole("ADMINISTRADOR") || gestor != null){
+			if(gestor != null){
 			var gestorModel = new GestaoPapelModel
 			{
 				Evento = _eventoService.GetEventoSimpleDto(idEvento),
