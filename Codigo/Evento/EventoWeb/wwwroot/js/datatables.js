458029fc764e0a85a1fc588a9219d5efd1e22409
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2026-06-23 13:58:12 -0300

    Adicionando DataTables ao index de Pessoa
    Atribuindo um ID à tabela e ajustes de layout à página de listagem de pessoas, além de incluir a inicialização do DataTables.
    
    - `id="tablePessoa"` foi adicionado à tabela para permitir sua manipulação pelo DataTables.
    - Criei o arquivo `wwwroot/js/datatables.js` para inicializar o DataTables para o id `#tablePessoa` (configurações de ordenação, definições de coluna, busca e paginação) e para carregar o arquivo de idioma em português.
    - Os botões de ação da linha foram envolvidos em um contêiner flexível responsivo com espaçamento.
    
    Nota:  Caso queira usar o arquivo `datatables.js`  em seu caso de uso, inclua o  `datatables.js`  na página ou no layout do seu caso de uso. #578

---
+++ Codigo/Evento/EventoWeb/wwwroot/js/datatables.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+	$('#tablePessoa').DataTable({
+		order: [[1, 'asc']],
+		columnDefs: [
+			{ orderable: true, targets: [1] },
+			{ orderable: false, targets: [0,2] }
+		],
+		searching: true,
+		ordering: true,
+		paging: true,
+		language: {
+			url: '@Url.Content("~/lib/datatable/js/pt-BR.json")'
+		}
+	});
+});
