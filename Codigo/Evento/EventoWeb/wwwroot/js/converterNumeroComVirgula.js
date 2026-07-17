2ea8f6ab86d09cfc1a8aae5527334d7d62057f6a
Ana Maria De Carvalho Mendonça <134330474+annymarry51@users.noreply.github.com>
2026-07-17 10:02:55 -0300

    Update converterNumeroComVirgula.js

--- Codigo/Evento/EventoWeb/wwwroot/js/converterNumeroComVirgula.js
+++ Codigo/Evento/EventoWeb/wwwroot/js/converterNumeroComVirgula.js
@@ -1,36 +1,21 @@
-﻿$(document).ready(function () {
-
-    function gerenciarCampoMonetario(idTela, idReal) {
-
-        const campoTela = document.getElementById(idTela);
-
-        const campoReal = document.getElementById(idReal);
-
-        if (!campoTela || !campoReal) return;
-
-        campoTela.addEventListener('input', function (e) {
-
-            let value = e.target.value.replace(/\D/g, '');
-
-            if (value.length > 0) {
-
-                value = (parseFloat(value) / 100).toFixed(2).replace('.', ',');
-
-            } else {
-
-                value = '0,00';
-
-            }
-
-            e.target.value = value;
-
-            campoReal.value = value;
-
-        });
-
-    }
-
-    gerenciarCampoMonetario("ValorTela", "ValorReal");
-    gerenciarCampoMonetario('ValorInscricao_Tela', 'ValorInscricao_Real');
-    gerenciarCampoMonetario('FrequenciaMinimaCertificado_Tela', 'FrequenciaMinimaCertificado_Real');
-});
+﻿console.log("arquivo JS carregado");
+function gerenciarCampoMonetario(idTela, idReal) {
+    const campoTela = document.getElementById(idTela);
+    const campoReal = document.getElementById(idReal);
+    if (!campoTela || !campoReal) return;
+    campoReal.value = campoTela.value;
+    campoTela.addEventListener('input', function (e) {
+        let value = e.target.value.replace(/\D/g, '');
+        if (value.length > 0) {
+            value = (parseFloat(value) / 100).toFixed(2).replace('.', ',');
+        } else {
+            value = '0,00';
+        }
+        e.target.value = value;
+        campoReal.value = value;
+        console.log(campoReal.value);
+    });
+}
+gerenciarCampoMonetario("ValorTela", "ValorReal");
+gerenciarCampoMonetario('ValorInscricao_Tela', 'ValorInscricao_Real');
+gerenciarCampoMonetario('FrequenciaMinimaCertificado_Tela', 'FrequenciaMinimaCertificado_Real');
