a391f1ff3d8c6f5c79dca339c9d7f7f885f85432
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2026-07-29 10:59:05 -0300

    feat(js): problema de formatação do cpf na página definir administrador.
    Foi adicionado em wwwroot\js o arquivo FormatarCpfCadastro.js, que contém duas funções. A função 'formatarCpfForm(campo)' formata  o cpf na view, adicionando os caracteres '.' e '-' a cada número que for digitado.  Já 'removerMascara()' substitui os caracteres '.' e '-' por "". #595

---
+++ Codigo/Evento/EventoWeb/wwwroot/js/FormatarCpfCadastro.js
@@ -0,0 +1,17 @@
+﻿function formatarCpfForm(campo) {
+
+    let cpf = campo.value.replace(/\D/g, "");
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    campo.value = cpf;
+}
+
+function removerMascara() {
+    let cpf = document.getElementById("Cpf");
+    if (cpf) {
+        cpf.value = cpf.value.replace(".", "");
+        cpf.value = cpf.value.replace(".", "");
+        cpf.value = cpf.value.replace("-", "");
+    }
+}
