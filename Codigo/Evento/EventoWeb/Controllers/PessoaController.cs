03b8587b1449583fb22600a2a318795438fdf1b2
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2026-08-27 20:40:46 -0300

    fix(controller): Valida modal
    Validando o modal antes de usar os métodos de validação e de criar uma 'Pessoa'. Isso corrige o erro
    'ArgumentNullException' quando o email for nulo. #637

--- Codigo/Evento/EventoWeb/Controllers/PessoaController.cs
+++ Codigo/Evento/EventoWeb/Controllers/PessoaController.cs
@@ -335,18 +335,17 @@ namespace EventoWeb.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DefinirAdministrador(GestaoAdministradorModel viewModel)
         {
-           
-            if (! _pessoaService.ValidaEmail(viewModel.Email))
+            if (ModelState.IsValid)
             {
-                ModelState.AddModelError("Email", "Por favor, digite um e-mail em um formato válido.");
-            }
-            else if (await _pessoaService.EmailExist(viewModel.Email, viewModel.Cpf))
-            {
-                ModelState.AddModelError("Email", "O e-mail informado já está em uso.");
-            }
-            else{
-
-                if (ModelState.IsValid)
+                if (!_pessoaService.ValidaEmail(viewModel.Email))
+                {
+                    ModelState.AddModelError("Email", "Por favor, digite um e-mail em um formato válido.");
+                }
+                else if (await _pessoaService.EmailExist(viewModel.Email, viewModel.Cpf))
+                {
+                    ModelState.AddModelError("Email", "O e-mail informado já está em uso.");
+                }
+                else
                 {
                     var pessoa = new Pessoa
                     {
@@ -378,7 +377,9 @@ namespace EventoWeb.Controllers
                     return RedirectToAction(nameof(DefinirAdministrador));
 
                 }
+
             }
+
             var adminsAtuais = await _pessoaService.GetAllAdmAsync();
             viewModel.Administradores = _mapper.Map<List<PessoaModel>>(adminsAtuais.OrderBy(p => p.Nome).ToList());
             return View(viewModel);
