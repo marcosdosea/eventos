c6dbfc466f16ce904c25db9f342654c4729441cc
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2026-09-06 23:55:24 -0300

    Refactor(view): Adaptar o modal de exclusão e mensagem de notificação.
    - Usando o atributo `data-nome`  para exibir no modal de exclusão, o atributo obtém o campo NomeCracha, que consiste no nome que é exibido no crachá de uma pessoa. Foi implementado um novo método em modal.js para identificar pelo id quando o 'modalExcluir' for executado, para substituir valor de `data-nome` na mensagem do modal de exclusão, caso não exista valor  vai substituir o nome pela string 'essa pessoa'. O NomeCracha é formatado para cortar do inicio da string até o primeiro espaço e para deixar a primeira letra maiúscula e as outras letras em minúsculas.
    - Melhorando e personalizando a mensagem exibida no modal #modalExcluir. As mensagens estão mais claras sobre a função da opção de 'excluir' e as mensagens são adaptadas para cada role do sistema.
    - Adicionando o arquivo `~/js/time_message.js` no `_LayoutGestor`. #669

--- Codigo/Evento/EventoWeb/wwwroot/js/modal.js
+++ Codigo/Evento/EventoWeb/wwwroot/js/modal.js
@@ -5,5 +5,27 @@
         if (form != null && modal != null) {
             modal.find("form").attr("action", form.attr("action"));
         }
+
+        
     }
+}
+const modalExcluir = document.getElementById('modalExcluir');
+if (modalExcluir) {
+    modalExcluir.addEventListener('show.bs.modal', function (event) {
+        const botao = event.relatedTarget;
+        const nome = botao.getAttribute('data-nome').split(' ')[0];
+        const nomeFormatado = nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase();
+       
+        const nomeItem = modalExcluir.querySelector('.nome-item');
+        if (nomeItem) {
+            if (nomeFormatado) {
+                nomeItem.textContent = nomeFormatado;
+            } else {
+                nomeItem.textContent = 'essa pessoa';
+            }
+
+        } else {
+            console.warn("O elemento '.nome-item' não foi encontrado no modal.");
+        }
+    });
 }
