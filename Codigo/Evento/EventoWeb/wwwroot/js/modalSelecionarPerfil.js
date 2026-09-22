01ca0c8646c81a1ca0117ea292239b6ec16013d8
Beatriz Carvalho <112733336+BeatrizSouz@users.noreply.github.com>
2026-09-22 11:39:29 -0300

    Feat: Exibir modal de seleção de perfil ao carregar
    Adicionando um script de modal do Bootstrap que abre automaticamente o modal de seleção de perfil após o carregamento da página, caso o elemento modal exista. Isso aprimora o fluxo de escolha de perfil ao solicitar a ação do usuário assim que a página estiver pronta. #675

---
+++ Codigo/Evento/EventoWeb/wwwroot/js/modalSelecionarPerfil.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+        var modalElement = document.getElementById('modalEscolherPerfil');
+        if (modalElement) {
+            var myModal = new bootstrap.Modal(modalElement);
+            myModal.show();
+        }
+});
+
